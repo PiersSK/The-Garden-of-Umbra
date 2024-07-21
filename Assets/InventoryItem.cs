727635127b2153140677a7ee7f942e3f2c454f52
OrcaOfTime <mwilliams.sean@gmail.com>
2024-07-21 14:27:18 +0100

    Inventory scripts and temp assets
    Inventory bar, slot and item to hold item data along with some temp asset sprites for item images

---
+++ Assets/InventoryItem.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Item item;
+
+    [Header("UI")]
+    public Image image;
+
+    [HideInInspector]
+    public Transform parentAfterDrag;
+
+    public void Start()
+    {
+        initialiseItem(item);
+    }
+
+    public void initialiseItem(Item newItem)
+    {
+        image.sprite = newItem.image;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false;
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
+    }
+}
