7f17de6def1c54e9aaea476d57abb9bb33a851ca
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2024-07-26 19:38:38 +0100

    UI updates (#21)
    * updates potion bottle UI
    
    * adds aspects to overworld creatures
    
    * adds shadow size to interact prompt
    
    * adds different flask icons

--- Assets/InventoryItem.cs
+++ Assets/InventoryItem.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -9,7 +10,13 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     [Header("UI")]
     public Image image;
     public GameObject releaseButton;
+    public GameObject aspectInfo;
     public Image releaseTimerImage;
+
+    public Image headAspect;
+    public Image bodyAspect;
+    public Image feetAspect;
+    public TextMeshProUGUI shadowName;
 
     [Header("Release Logic")]
     public float timeToRelease = 2f;
@@ -36,6 +43,8 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
                 item.RemoveShadow();
 
                 releaseButton.SetActive(false);
+                aspectInfo.SetActive(false);
+
                 releaseInProgress = false;
                 releaseTimerImage.fillAmount = 0f;
                 releaseTimer = 0f;
@@ -43,21 +52,46 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         }   
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        //transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
-        //transform.localEulerAngles = new Vector3(0, 0, 30f);
+    private void UpdateShadowAspectUI() {
 
-        if(item.shadow is not null)
-            releaseButton.SetActive(true);
+        headAspect.sprite = item.shadow.headAspectSprite;
+        headAspect.color = item.shadow.headAspect == Aspects.HeadAspect.None ? new Color(0,0,0,0.2f) : Color.white;
+
+        bodyAspect.sprite = item.shadow.bodyAspectSprite;
+        bodyAspect.color = item.shadow.bodyAspect == Aspects.BodyAspect.None ? new Color(0, 0, 0, 0.2f) : Color.white;
+
+        feetAspect.sprite = item.shadow.feetAspectSprite;
+        feetAspect.color = item.shadow.feetAspect == Aspects.FeetAspect.None ? new Color(0, 0, 0, 0.2f) : Color.white;
+
+        shadowName.text = item.shadow.shadowName;
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //transform.localEulerAngles = new Vector3(0, 0, 30f);
+
+        if (item.shadow is not null)
+        {
+            UpdateShadowAspectUI();
+
+            transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+            releaseButton.SetActive(true);
+            aspectInfo.SetActive(true);
+            Debug.Log(item.shadow.headAspect);
+            Debug.Log(item.shadow.bodyAspect);
+            Debug.Log(item.shadow.feetAspect);
+        }
+    }
+
+        public void OnPointerExit(PointerEventData eventData)
     {
         //transform.localScale = Vector3.one;
         //transform.localEulerAngles = idleAngle;
         if (item.shadow is not null)
+        {
             releaseButton.SetActive(false);
+            aspectInfo.SetActive(false);
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
