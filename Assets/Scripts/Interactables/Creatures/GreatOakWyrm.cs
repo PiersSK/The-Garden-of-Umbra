440f11a22a7ee912eb4d0a6c809e9783f29901e3
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-07-31 01:35:12 +0100

    remove unnecessary debug help

--- Assets/Scripts/Interactables/Creatures/GreatOakWyrm.cs
+++ Assets/Scripts/Interactables/Creatures/GreatOakWyrm.cs
@@ -5,12 +5,9 @@ public class GreatOakWyrm : ShadowInteractable
 {
     public bool isUnderTree;
     public GameObject questionMarker;
-    public bool isQuestionEnabled;
-
     protected override void Start()
     {
         base.Start();
-        isQuestionEnabled = questionMarker.activeSelf;
     }
 
     public override bool CanInteract()
