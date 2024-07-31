d2b9e4c3589935732030439c26aa21b2c6d2432b
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-07-31 01:33:52 +0100

    adds question mark for our friend

--- Assets/Scripts/Interactables/Creatures/GreatOakWyrm.cs
+++ Assets/Scripts/Interactables/Creatures/GreatOakWyrm.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 public class GreatOakWyrm : ShadowInteractable
 {
     public bool isUnderTree;
-    public GameObject surpriseMarker;
+    public GameObject questionMarker;
+    public bool isQuestionEnabled;
 
     protected override void Start()
     {
         base.Start();
-        surpriseMarker.SetActive(false);
+        isQuestionEnabled = questionMarker.activeSelf;
     }
 
     public override bool CanInteract()
     {
         return base.CanInteract() && !isUnderTree;
     }
-
-
 
 }
